f7d1cc3c0771b9dd678eae291a58de21e8a8f006
Amworkman <adammworkman25@gmail.com>
2020-07-29 20:32:43 -0400

    added columns

--- db/migrate/20200728212518_create_records.rb
+++ db/migrate/20200728212518_create_records.rb
@@ -2,8 +2,16 @@ class CreateRecords < ActiveRecord::Migration[5.2]
   def change
     create_table :records do |t|
       t.string :title
+      title.string :barcode
+      t.string :catalog_number
       t.integer :artist_id
       t.integer :label_id
+      t.interger :discogs_id
+      t.string :image_url
+      t.string :genres
+      t.string :year
+      t.string :lowest_price
+      t.string :num_for_sale
     end
   end
 end
