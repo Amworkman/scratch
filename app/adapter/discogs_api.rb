aab3ea08a7b7075fd800273768b1c5479d742ae0
Amworkman <adammworkman25@gmail.com>
2020-08-09 17:26:59 -0400

    bug fix

--- app/adapter/discogs_api.rb
+++ app/adapter/discogs_api.rb
@@ -1,19 +1,20 @@
-Model DiscogsAPI
-
-  @@root_url = "https://api.discogs.com/database/search?q="
-  @@discogs_key = "&key=#{ENV[:key]}&secret=#{ENV[:secret]}"
+class DiscogsAPI
+  
+  @@root_url = "https://api.discogs.com"
+  @@discogs_key = ENV["dk"]
+  
 
   def search_from_catalog_number(catno)
-    search = HTTParty.get("#{@@root_url}/database/search?q=#{catno}#{@@discogs_key}")
-    #set discogs_id to "master_id"
-  end
-
-  def search_from_barcode(barcode)
-    search = HTTParty.get("#{root_@@url}/database/search?q=#{barcode}#{@@discogs_key}")
-    #set discogs_id to "master_id"
+    search = HTTParty.get("#{@@root_url}/database/search?q=#{catno}#{@@discogs_key}&per_page=100")
+    search["results"]
   end
 
   def get_info(discogs_id)
-    info = "#{@@root_url}/masters/#{discogs_id}"
+    info = HTTParty.get"#{@@root_url}/releases/#{discogs_id}"
+  end
+
+  def get_images(discogs_id)
+    info = HTTParty.get"#{@@root_url}/database/search?release_id=#{discogs_id}#{@@discogs_key}"
+  end
 
 end
