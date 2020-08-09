93e4c4bb8ec7360532fc8c6d5f59b94664cee2d7
Amworkman <adammworkman25@gmail.com>
2020-08-09 17:27:19 -0400

    added helpers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,26 @@
 class ApplicationController < Sinatra::Base
 
   configure do
+    enable :sessions
+    set :session_secret, "My session secret"
     set :public_folder, 'public'
     set :views, './app/views'
+    helpers Sinatra::Cookies
   end
 
+  
+  helpers do
+
+    def signed_in? 
+       !!session[:user_id]
+    end
+
+    def current_user
+      @user ||= User.find(session[:user_id])
+    end
+  end
+    
   get '/' do
-
+    erb :'records/index'
   end
-
 end
