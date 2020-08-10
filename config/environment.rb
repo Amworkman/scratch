c99a567ce01fd861cf9ca47aa28d159fe440b9cd
Amworkman <adammworkman25@gmail.com>
2020-08-10 12:34:57 -0400

    prepare for deployment

--- config/environment.rb
+++ config/environment.rb
@@ -7,9 +7,9 @@ require 'sinatra'
 
 
 
-ActiveRecord::Base.establish_connection(
-  :adapter => "sqlite3",
-  :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
-)
+ActiveRecord::Base.establish_connection( ENV['DATABASE_URL'] || {
+  adapter: "sqlite3",
+  database: "db/#{ENV['SINATRA_ENV']}.sqlite"
+})
 
 require_all 'app'
